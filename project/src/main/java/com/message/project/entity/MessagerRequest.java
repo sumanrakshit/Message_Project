64667b011592e0d35ae1c14a8690e07c866192cd
suman rakshit <sumanrakshit0305@gmail.com>
2024-03-15 13:05:34 +0530

    message req

--- project/src/main/java/com/message/project/entity/MessagerRequest.java
+++ project/src/main/java/com/message/project/entity/MessagerRequest.java
@@ -1,5 +1,54 @@
 package com.message.project.entity;
 
 public class MessagerRequest {
+	
+	private String date;
+	private String author;
+	private String message;
+	private String attachment;
+	private String signature;
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getAttachment() {
+		return attachment;
+	}
+	public void setAttachment(String attachment) {
+		this.attachment = attachment;
+	}
+	public String getSignature() {
+		return signature;
+	}
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+	public MessagerRequest(String date, String author, String message, String attachment, String signature) {
+		super();
+		this.date = date;
+		this.author = author;
+		this.message = message;
+		this.attachment = attachment;
+		this.signature = signature;
+	}
+	public MessagerRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
 
 }
